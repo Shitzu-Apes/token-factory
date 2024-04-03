074fc0ada9ba556e99784d1d5f43a4c19e2d9e6e
pysrxyz <gm@pysr.xyz>
2024-04-03 16:00:49 +0700

    feat: fetch pool data from ref contract

--- frontend/src/lib/useTokens.tsx
+++ frontend/src/lib/useTokens.tsx
@@ -1,10 +1,26 @@
 import { useEffect, useState } from 'react';
 import { useNearWallet } from './useNearWallet';
-import { ContractName, localStorageKeyCachedTokens } from './constant';
+import {
+  ContractName,
+  RefContractId,
+  SimplePool,
+  localStorageKeyCachedTokens,
+  wNEAR
+} from './constant';
 import { TokenArgs } from '../pages/Tokens/components/OptionsSection/OptionsSection';
+
+type TPool = {
+  [token_contract: string]: {
+    price: number;
+    liquidity: bigint;
+    fee: number;
+    index: number;
+  };
+};
 
 export function useTokens(wallet: ReturnType<typeof useNearWallet>) {
   const [tokens, setTokens] = useState<TokenArgs[]>([]);
+  const [pools, setPools] = useState<TPool>({});
 
   useEffect(() => {
     (async () => {
@@ -22,7 +38,6 @@ export function useTokens(wallet: ReturnType<typeof useNearWallet>) {
       const limit = 100;
       const getTokensPromises = [];
       for (let i = cachedTokens.length; i < numTokens; i += limit) {
-        console.log({ i, numTokens });
         getTokensPromises.push(
           wallet.viewMethod({
             contractId: ContractName,
@@ -42,5 +57,79 @@ export function useTokens(wallet: ReturnType<typeof useNearWallet>) {
     })();
   }, [wallet.provider]);
 
-  return { tokens };
+  useEffect(() => {
+    (async () => {
+      if (wallet.provider === null) return;
+      if (tokens.length === 0) return;
+      const numPools = await wallet.viewMethod({
+        contractId: RefContractId,
+        method: 'get_number_of_pools',
+        args: {}
+      });
+
+      const promises = [];
+      const limit = 1_000;
+      for (let i = 0; i < numPools; i += limit) {
+        promises.push(
+          wallet.viewMethod({
+            contractId: RefContractId,
+            method: 'get_pools',
+            args: { from_index: i, limit }
+          })
+        );
+      }
+
+      const rawPools: {
+        pool_kind: string;
+        token_account_ids: string[];
+        amounts: string[];
+        total_fee: string;
+        shares_total_supply: string;
+      }[] = (await Promise.all(promises)).flat();
+
+      const pools: {
+        [token_contract: string]: {
+          price: number;
+          liquidity: bigint;
+          fee: number;
+          index: number;
+        };
+      } = {};
+
+      rawPools.forEach((pool, i) => {
+        if (pool.pool_kind === SimplePool) {
+          const nearIdx = pool.token_account_ids.indexOf(wNEAR);
+          const token_contract = pool.token_account_ids[1 - nearIdx];
+          if (nearIdx === -1) return;
+
+          const nearAmount = BigInt(pool.amounts[nearIdx]);
+          const tokenAmount = BigInt(pool.amounts[1 - nearIdx]);
+
+          if (token_contract === 'intel.tkn.near') {
+            console.log({ nearAmount, tokenAmount });
+          }
+
+          if (nearAmount === BigInt(0) || tokenAmount === BigInt(0)) return;
+
+          const liquidity = BigInt(2) * nearAmount;
+
+          if (token_contract in pools && pools[token_contract].liquidity > liquidity) return;
+
+          const fee = Number(pool.total_fee);
+          const price = Number(nearAmount) / Number(tokenAmount);
+
+          pools[token_contract] = {
+            price,
+            liquidity,
+            fee,
+            index: i
+          };
+        }
+      });
+
+      setPools(pools);
+    })();
+  }, [tokens]);
+
+  return { tokens, pools };
 }
--- frontend/src/pages/Tokens/components/TokensSection/TokensSection.tsx
+++ frontend/src/pages/Tokens/components/TokensSection/TokensSection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 // import PaginationBox from '../../../../components/elements/PaginationBox';
 // import Table from '../../../../components/elements/Table';
@@ -17,10 +17,62 @@ const rowsPerPage = 50;
 
 function TokensSection({ isDarkMode }: { isDarkMode: boolean }) {
   const wallet = useNearWalletContext();
+  const [sortedBy, setSortedBy] = useState(SortedByLiquidity);
 
-  const { tokens } = useTokens(wallet);
+  const { tokens, pools } = useTokens(wallet);
 
-  return <div className={''}>{JSON.stringify(tokens)}</div>;
+  return (
+    <div className={''}>
+      <div className={''}>
+        <span className={`padding-20-20-0-0 ${isDarkMode && 'color-white'}`}>{'Sort by'}</span>
+        <div className="btn-group" role="group" aria-label="Sorted By">
+          <button
+            type="button"
+            className={`btn ${
+              sortedBy === SortedByLiquidity ? 'btn-secondary background-color-black' : 'btn'
+            }`}
+            onClick={() => setSortedBy(SortedByLiquidity)}
+          >
+            Liquidity
+          </button>
+          {wallet.accountId && (
+            <button
+              type="button"
+              className={`btn ${
+                sortedBy === SortedByYourTokens ? 'btn-secondary background-color-black' : 'btn'
+              }`}
+              onClick={() => setSortedBy(SortedByYourTokens)}
+            >
+              Your tokens
+            </button>
+          )}
+          <button
+            type="button"
+            className={`btn ${
+              sortedBy === SortedByIndex ? 'btn-secondary background-color-black' : 'btn'
+            }`}
+            onClick={() => {
+              setSortedBy(SortedByIndex);
+            }}
+          >
+            Index
+          </button>
+        </div>
+      </div>
+      <div className={''}>
+        <Table columns={columns} data={currentData} isDarkMode={isDarkMode} />
+      </div>
+      <div className={''}>
+        <PaginationBox
+          handlePage={this.props.handlePage}
+          rowsPerPage={rowsPerPage}
+          dataLength={dataFiltered.length}
+          currentPage={this.props.currentPage}
+          isDarkMode={isDarkMode}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default React.memo(TokensSection);
